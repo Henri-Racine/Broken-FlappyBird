806b5b0dac6fa33bd18d420bf95bd474d8743fdd
Diendophyx <42424060+Diendophyx@users.noreply.github.com>
2019-05-17 22:46:08 +1000

    Initial commit

---
+++ Assets/Scripts/Player/UserInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlappyBird
+{
+    [RequireComponent(typeof(Bird))]
+    public class UserInput : MonoBehaviour
+    {
+        private GameObject bird;
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            var bird = GetComponent<Bird>();
+            // Check for mouse down
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Flap the bird
+                bird.Flap();
+            }
+        }
+    }
+}
